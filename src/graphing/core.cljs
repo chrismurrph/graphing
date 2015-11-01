9f8c5ed31c7aac52ba44bc049982bb17e24561a4
chris <chris@strandz.org>
2015-11-01 15:07:29 +1100

    Moving on to try graphing although most of it is commented out

--- src/graphing/core.cljs
+++ src/graphing/core.cljs
@@ -13,10 +13,25 @@
 (def uniqkey (atom 0))
 (defn gen-key [] 
   (let [res (swap! uniqkey inc)]
-    (u/log res)
+    ;(u/log res)
     res))
 
-(defn controller [inchan state-ref]
+(def point-defaults
+  {:stroke "black"
+   :stroke-width 2
+   :fill "blue"
+   :r 5})
+
+;;
+;; Creates a point as a component
+;;
+(defn point [x y]
+  [:circle
+   (merge point-defaults
+          {:cx x
+           :cy y})])
+
+(defn old-controller [inchan state-ref]
   (go-loop [cur-x nil cur-y nil mouse-state :up]
     (match [(<! inchan) mouse-state]
            [({:type "mousedown" :x x :y y} :as e) :up]
@@ -35,12 +50,39 @@
              (recur x y :up))
 
            [s e] (recur cur-x cur-y mouse-state))))
-    
-(defn path-component [its-path fill-color]
+
+(defn new-controller [inchan state-ref]
+  (go-loop [cur-x nil cur-y nil mouse-state :up]
+           (match [(<! inchan) mouse-state]
+
+                  [({:type "mousemove" :x x :y y} :as e) :up]
+                  (do
+                    ;(u/log "Moved to " x " " y)
+                    (recur x y :up))
+
+                  [({:type "mouseup" :x x :y y} :as e) :up]
+                  (do
+                    (swap! state-ref (fn [{:keys [current-path paths my-points] :as state}]
+                                       (assoc state :my-points (conj my-points [x y]))))
+                    (u/log (:my-points @state-ref))
+                    (recur x y :up))
+
+                  [s e] (recur cur-x cur-y mouse-state))))
+
+(defn old-path-component [its-path fill-color]
    (let [xys (map (fn [{:keys [x y]}] (str x " " y)) its-path)
          points (apply str (interpose ", " xys))
-         _ (u/log points)]
-     [:polyline {:key (gen-key) :points points :stroke fill-color :fill "none"}]))     
+         idx (gen-key)
+         _ (u/log idx " " (count points))
+         ]
+     [:polyline {:key idx :points points :stroke fill-color :fill "none"}]))
+
+(defn new-path-component [its-path fill-color]
+  (into [:polyline]
+        (for [xys (map (fn [{:keys [x y]}] (str x " " y)) its-path)
+              points (apply str (interpose ", " xys))
+              idx (gen-key)]
+          {:key idx :points points :stroke fill-color :fill "none"})))
 
 (defn event-handler-fn [comms component e]
   (let [bounds (. (reagent/dom-node component) getBoundingClientRect)
@@ -49,23 +91,28 @@
      (put! comms {:type (.-type e) :x x :y y})
      nil))
 
+(defn my-points-component []
+  [:g
+   [point 10 10]
+   [point 20 20]])
+
 (defn trending-app [{:keys [state-ref comms] :as props}]
-  (let [{:keys [paths current-path]} @state-ref
+  (let [{:keys [paths my-points current-path]} @state-ref
         component (reagent/current-component)
-        handler-fn (partial event-handler-fn comms component)]
-    [:svg {:key (gen-key) 
-           :height 480 :width 640 
-           :on-mouse-up handler-fn :on-mouse-down handler-fn :on-mouse-move handler-fn
-           :style {:border "thin solid black"}}
-     (cons [path-component current-path "red"]
-      (map #(vector path-component % "black") paths))]))
+        handler-fn (partial event-handler-fn comms component)
+        ]
+    [:svg {:height 480 :width 640 :on-mouse-up handler-fn :on-mouse-down handler-fn :on-mouse-move handler-fn :style {:border "thin solid black"}}
+     (cons [new-path-component current-path "red"]
+      (map #(vector new-path-component % "black") paths))
+      ;[my-points-component]
+      ]))
       
 (defn mount-root []
-  (let [paths-ratom (ratom {:paths []})
+  (let [paths-ratom (ratom {:paths [] :my-points []})
         ch (chan)
-        proc (controller ch paths-ratom)]
+        proc (old-controller ch paths-ratom)]
     (reagent/render-component
-       [trending-app {:key (gen-key) :state-ref paths-ratom :comms ch}]
+       [trending-app {:state-ref paths-ratom :comms ch}]
        (.-body js/document))
     (go
       (let [exit (<! proc)]
