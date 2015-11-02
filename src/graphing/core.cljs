f3681bbe002507e616ddce0dcd5573f605dc9017
chris <chris@strandz.org>
2015-11-02 23:33:09 +1100

    Can put whole lines in from temp buttons. Next is for the lines that are graphed to have :name and :colour

--- src/graphing/core.cljs
+++ src/graphing/core.cljs
@@ -3,6 +3,7 @@
             [cljs.core.async :as async
              :refer [<! >! chan close! sliding-buffer put! alts!]]
             [cljs.core.match]
+            [graphing.graph-lines-db :as db]
             [graphing.utils :as u])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]
                    [cljs.core.match.macros :refer [match]]))
@@ -17,7 +18,6 @@
     res))
 
 (def first-line [[10 10] [20 20] [30 30] [40 40] [50 50]])
-(def current-line 1)
 (def height 480)
 (defn now-time [] (js/Date.))
 (defn seconds [js-date] (.getSeconds js-date))
@@ -41,7 +41,6 @@
 (def point-defaults
   {:stroke "black"
    :stroke-width 2
-   :fill "blue"
    :r 5})
 
 (def segment-defaults
@@ -67,11 +66,12 @@
 ;;
 ;; Creates a point as a component
 ;;
-(defn point [x y]
+(defn point [fill x y]
   [:circle
    (merge point-defaults
           {:cx x
-           :cy y})])
+           :cy y
+           :fill fill})])
 
 (defn old-controller [inchan state-ref]
   (go-loop [cur-x nil cur-y nil mouse-state :up]
@@ -92,6 +92,9 @@
              (recur x y :up))
 
            [s e] (recur cur-x cur-y mouse-state))))
+
+(def state (ratom {:my-lines [first-line] :hover-pos nil :last-mouse-moment nil}))
+(def current-line (count (:my-lines @state)))
 
 (defn new-controller [inchan state-ref]
   (go-loop [cur-x nil cur-y nil]
@@ -116,7 +119,6 @@
 
                   [_]
                   (do
-                    (u/log "Is constant or what?")
                     (recur cur-x cur-y)))))
 
 (def controller new-controller)
@@ -128,14 +130,12 @@
      (put! comms {:type (.-type e) :x x :y y})
      nil))
 
-(defn point-component [[x y]]
-  ^{:key (gen-key)} [point x y])
+(defn point-component [fill [x y]]
+  ^{:key (gen-key)} [point fill x y])
 
 (defn all-points-component [my-lines]
   (into [:g {:key (gen-key)}]
-        (map #(point-component %) (mapcat identity my-lines))))
-
-(def state (ratom {:my-lines [first-line] :hover-pos nil :last-mouse-moment nil}))
+        (map #(point-component nil %) (mapcat identity my-lines))))
 
 (defn hover-visible? [last-mouse-moment now-moment]
   (if (nil? last-mouse-moment)
@@ -154,16 +154,34 @@
 
 (defonce time-updater (js/setInterval #(tick) 100))
 
+(defn read-in-line [name]
+  (u/log name)
+  (let [line (db/get-line name)
+        colour (:colour line)
+        positions (:positions line)
+        mapify-fn (fn [{x :x y :y}] [x y])
+        mapped-in (mapv mapify-fn positions)
+        count-existing-lines (count (:my-lines @state))]
+    (u/log mapped-in " where are already " count-existing-lines)
+    (swap! state assoc-in [:my-lines count-existing-lines] mapped-in)
+    (swap! state assoc-in [:my-lines count-existing-lines] colour)))
+
 (defn trending-app [{:keys [state-ref comms] :as props}]
   (let [{:keys [my-lines hover-pos last-mouse-moment]} @state-ref
         component (reagent/current-component)
         handler-fn (partial event-handler-fn comms component)
         ]
-    [:svg {:height height :width 640
-           :on-mouse-up handler-fn :on-mouse-down handler-fn :on-mouse-move handler-fn
-           :style {:border "thin solid black"}}
-     [all-points-component my-lines]
-     [hover-line-at (not (nil? hover-pos)) hover-pos]]))
+    [:div
+     [:svg {:height height :width 640
+            :on-mouse-up handler-fn :on-mouse-down handler-fn :on-mouse-move handler-fn
+            :style {:border "thin solid black"}}
+      [all-points-component my-lines]
+      [hover-line-at (not (nil? hover-pos)) hover-pos]]
+     [:input {:type "button" :value "Methane"
+              :on-click (partial read-in-line "Methane")}]
+     [:input {:type "button" :value "Oxygen"
+              :on-click (partial read-in-line "Oxygen")}]]
+  ))
 
 (defn mount-root []
   (let [paths-ratom state
---
+++ src/graphing/graph_lines_db.cljs
@@ -0,0 +1,118 @@
+(ns ^:figwheel-always graphing.graph-lines-db
+  (:require [graphing.utils :as u]))
+
+(def blue {:r 0 :g 51 :b 102})
+(def light-blue {:r 0 :g 204 :b 255})
+(def pink {:r 255 :g 0 :b 255})
+(def brown {:r 102 :g 51 :b 0})
+(def green {:r 0 :g 102 :b 0})
+(def red {:r 255 :g 0 :b 0})
+(def gray {:r 64 :g 64 :b 64})
+
+(def local-mouse-pos (atom [0 0]))
+(def last-time-mouse-moved (atom 0))
+
+;;
+;; All the lines that get graphed. Has nothing to do with Reagent, so use a normal atom.
+;; :x and :y are both always in the range 0-999. Scaling to the actual graph enclosure is done at drawing time
+;; i.e. completely dynamically.
+;; Note usual computer/graphical rules apply, so x starts at 0 and goes across the top, and y then goes down from
+;; the top.
+;; Note that the x value is always increasing, meaning older come before newer, because x is time.
+;; TODO - change names here and at mouse-at to be keywords
+;;
+(def lines (atom [
+                  {:colour pink
+                   :name "Methane"
+                   :positions [{:x 20 :y 320 :val 0} {:x 51 :y 321 :val 0} {:x 82 :y 422 :val 0} {:x 104 :y 523 :val 0}
+                               {:x 115 :y 426 :val 0} {:x 227 :y 428 :val 0} {:x 320 :y 532 :val 0} {:x 344 :y 333 :val 0}
+                               {:x 435 :y 435 :val 0} {:x 440 :y 338 :val 0} {:x 541 :y 243 :val 0} {:x 644 :y 446 :val 0}]}
+                  {:colour green
+                   :name "Oxygen"
+                   :positions [{:x 40 :y 350 :val 0} {:x 141 :y 351 :val 0} {:x 242 :y 352 :val 0} {:x 344 :y 353 :val 0}
+                               {:x 445 :y 454 :val 0} {:x 546 :y 459 :val 0} {:x 548 :y 460 :val 0} {:x 650 :y 461 :val 0}
+                               {:x 651 :y 563 :val 0} {:x 653 :y 565 :val 0} {:x 755 :y 565 :val 0} {:x 857 :y 568 :val 0} {:x 960 :y 672 :val 0}]}]))
+
+;;
+;; As mousemove and has been there a small period of time then this is written to. Is then picked up by the draw function.
+;;
+(def mouse-at-x (atom 350))
+(def mouse-at-ys (atom [{:name "Hydrogen" :value 0.07 :y 300} {:name "Boron" :value 0.17 :y 310}]))
+
+(defn get-line [name]
+  (first (filter #(= name (-> % :name)) @lines)))
+
+;;
+;; Client will know the width and height of the area it needs to put dots onto. These will change and every time
+;; they do a new scale function can be requested here. Note that the world of this namespace is 0 - 999 inclusive.
+;; If height changes a new height partial function will be requested.
+;; At any time for drawing a dot on the actual canvas there will be two partial functions originally gained from
+;; here. `scale-height` for the y value and `scale-width` for the x value.
+;;
+(defn scale-fn [width-or-height]
+  (partial u/scale {:min 0 :max 999} {:min 0 :max width-or-height}))
+
+;;
+;; We are always going to get y duplicates so x are interesting. Two events s/not happen at the same time.
+;; There is no purpose to this function yet, just done for the sake of interest.
+;; First up we convert from vector to a vectors of hash-maps where key is the scaled x value. Then we can group-by this scaled
+;; x value. Then filter when more than 2 and return a vector of scaled x values. The size of this vector over the
+;; number of positions in the line tells how much squashing is going on.
+;;
+(defn find-x-duplicates [scaling-fn line-name]
+  (let [by-scaled-x (map #(vector (scaling-fn (:x %)) %) (:positions (get-line line-name)))
+        grouping-fn (fn [[k v]] k)
+        grouped (group-by grouping-fn by-scaled-x)
+        filter-fn (fn [[k v]] (> (count v) 1))
+        filtered (filter filter-fn grouped)
+        res (map first filtered)
+        ;_ (u/log by-scaled-x)
+        ;_ (u/log grouped)
+        ;_ (u/log filtered)
+        _ (u/log res)
+        ]
+    res))
+
+;;
+;; Any x may have two positions, one on either side, or none. These two positions will be useful to the drop-down
+;; y-line that comes up as the user moves the mouse over the graph.
+;; In the reduce implementation we only know the previous one when we have gone past it, hence we need to keep the
+;; prior in the accumulator.
+;; Because of the use-case, when we are exactly on it we repeat it. I'm thinking the two values will have the greatest
+;; or least used. This obviates the question of there being any preference for before or after. Also when user is at
+;; the first or last point there will still be a result.
+;;
+(defn enclosed-by [x line-name]
+  (let [line (get-line line-name)
+        positions (:positions line)
+        res (reduce (fn [acc ele] (if (empty? (:res acc))
+                                    (if (= (:x ele) x)
+                                      {:res [ele ele]}
+                                      (if (> (:x ele) x)
+                                        {:res [(:prev acc)] :prev ele} ;use the prior element
+                                        {:res [] :prev ele}) ;only update prior element
+                                    (if (= 1 (count (:res acc)))
+                                      {:res (conj (:res acc) (:prev acc))}
+                                      acc)))) ;have answer already
+                    []
+                    positions)
+        _ (u/log res)]
+    (if (empty? (:res res))
+      nil
+      res)))
+
+(defn get-names []
+  (map :name @lines))
+
+(defn remover [idx coll]
+  (u/vec-remove coll idx))
+
+(defn random-position-removal []
+  (let [line-idx (rand-int (count @lines))
+        so-line (nth @lines line-idx)
+        so-positions (:positions so-line)
+        positions-count (count so-positions)]
+    (when (pos? positions-count)
+      (let [pos-idx (rand-int positions-count)
+            remover-at-idx (partial remover pos-idx)]
+        (swap! lines update-in [line-idx :positions] remover-at-idx)))))
